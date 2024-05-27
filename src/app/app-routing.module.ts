2fa9e265542da023d1e8f1bea93359f1a26d08a2
agw123 <cosecosi8@gmail.com>
2024-05-27 20:58:53 +0200

    fix routing on refresh

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,17 +6,19 @@ import { ProjectsComponent } from './projects/projects.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+const suffixTitle = 'Agata Wisniewska Web Developer';
+
 const routes: Routes = [
-  { path: '',component: HomeComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'projects', component: ProjectsComponent },
-  { path: 'contact', component: ContactsComponent },
-  { path: '**', component: PageNotFoundComponent }
+  { path: '',component: HomeComponent, title: 'Home' + ' ' + suffixTitle },
+  { path: 'home', component: HomeComponent, title: 'Home' + ' ' + suffixTitle},
+  { path: 'about', component: AboutComponent, title: 'About' + ' ' + suffixTitle },
+  { path: 'projects', component: ProjectsComponent, title: 'Projects' + ' ' + suffixTitle },
+  { path: 'contact', component: ContactsComponent, title: 'Contact' + ' ' + suffixTitle },
+  { path: '**', component: PageNotFoundComponent, title: 'Page Not Found' + ' ' + suffixTitle }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: false })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
